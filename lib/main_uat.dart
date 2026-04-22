19e7da55e0b891623c54480bdd54872b152d684a
Hiepmc32 <72059432+Hiepmc32@users.noreply.github.com>
2026-04-22 09:32:09 +0700

    update main

--- lib/main_uat.dart
+++ lib/main_uat.dart
@@ -2,5 +2,6 @@ import 'package:fresh_base_project/base_run_main.dart';
 import 'package:fresh_base_project/core/config/uat.config.dart';
 
 Future<void> main() async {
+  //todo
   await BaseRunMain.runMainApp(config: UatConfig());
 }
